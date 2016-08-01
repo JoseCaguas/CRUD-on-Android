39f3770c9883ee554660f06a3e3eec83a25d7769
josecaguas <josecaguas@gmail.com>
2016-08-01 19:15:31 -0400

    completed the import of the code

--- app/src/main/java/com/example/sairamkrishna/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sairamkrishna/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.sairamkrishna.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-package com.example.sairamkrishna.myapplication;
+
 
 import android.content.Context;
 import android.content.Intent;
@@ -58,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
